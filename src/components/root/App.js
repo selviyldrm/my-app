0b81a4645db83c5013d0a8395ddd6cd22344dc32
Selvi Yildirim <selviyldrm96@gmail.com>
2022-05-17 21:58:37 +0300

    react-redux

---
+++ src/components/root/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Container } from 'reactstrap';
+import Navi from "../navi/Navi"
+import Dashboard from './Dashboard';
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <Navi />
+        <Dashboard />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import App from './components/root/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Provider } from 'react-redux';
+import configureStore from './redux/reducers/configureStore';
 
+const store=configureStore();
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <Provider store={store}><App/></Provider>
   </React.StrictMode>
 );
 
